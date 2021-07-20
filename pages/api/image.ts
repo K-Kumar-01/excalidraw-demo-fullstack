6430e59a1acfd8a29c5a5b9be036b4b159ac2d21
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2021-07-21 02:43:05 +0530

    feat(api): add image post method
    Signed-off-by: K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,68 +1,22 @@
-import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
-import { promises } from 'fs';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../utils';
-
-const { readFile, readdir, writeFile } = promises;
-
-const getallImages = async () => {
-  try {
-    let fileNames = await readdir('./public');
-    fileNames = fileNames.filter((name) => !name.endsWith('ico'));
-    let isErrorThere = false;
-    const filesContents = await Promise.all(
-      fileNames.map(async (file) => {
-        try {
-          const content = await readFile(`./public/${file}`, 'utf8');
-          return content;
-        } catch (error) {
-          isErrorThere = true;
-          return '';
-        }
-      })
-    );
-    if (isErrorThere) {
-      throw new Error('Error in reading files');
-    }
-    return filesContents;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
-const getSingleImage = async (imageName: string) => {
-  try {
-    const content = await readFile(`./public/${imageName}`, 'utf8');
-    return content;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+import ImageModel from '../../model';
 
 const image = async (req: NextApiRequest, res: NextApiResponse) => {
   await dbConnect();
 
   if (req.method === 'POST') {
-    let { svgString, name } = req.body;
-    svgString = JSON.parse(svgString);
-
     try {
-      const file = await writeFile(`./public/${name}.svg`, svgString);
-      res.status(200).json({ name });
+      const image = await ImageModel.findOneAndUpdate(
+        { name: req.body.name, link: req.body.link },
+        { ...req.body },
+        { new: true, upsert: true }
+      );
+      res.status(201).json({ success: true, data: image });
     } catch (error) {
-      res.status(500).end();
+      res.status(500);
     }
   } else if (req.method === 'GET') {
-    try {
-      if (!req.query.name) {
-        const allImages = await getallImages();
-        res.status(200).json({ allImages });
-      } else {
-        const image = await getSingleImage(req.query.name as string);
-        res.status(200).json({ image });
-      }
-    } catch (error) {
-      res.status(500).end();
-    }
   }
 };
 
